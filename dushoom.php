3b06aa2a8cbe5eedae0bf2eadc1bf11538e5416f
anjalibandaru <anjalibandaru513@gmail.com>
2017-04-07 15:41:57 +0530

    First commit

---
+++ dushoom.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>DUSHOOM</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="
+  style.css">
+ 
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+	<table class="table table-borderless">
+		<tbody>
+			<tr>
+				<td class="heading">
+					<h2>COMIC JUNCTION</h2>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<div id="header" class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img class=img-circle src="dishoom.jpg" >
+				<p class="head1" >DUSHOOM</p>
+				<p class="head2">CARTOON QUOTES FROM INSPIRATIONAL FOLKS</p>
+			</div>
+		</div>
+		<div class="col-md-12">
+			<div class="row strip"></div>
+		
+		<div class="row ">
+			
+				<img src="cartoon.jpeg">
+			</div>
+		</div>
+	</div>
+		<div class="container">
+		<div class="col-md-12  strip1">
+			<div class="row">
+				<p class="foot"> DUSHOOM.COM</p>
+			</div>
+		</div>
+		<div class="col-md-12 strip2">
+			<div class="row"></div>
+		</div>
+
+		<div class="col-md-12">
+			<div class="row ">
+			<div class="indent">
+				<h3 >207. STEPHEN KING: The desk</h3>
+				<p >By Gav on March 1, 2017</p>
+				<div class="fb-like" data-href="https://www.facebook.com/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
+				<div class="fb-share-button" data-href="https://www.facebook.com/" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2F&amp;src=sdkpreparse">Share</a></div>
+				<a href="https://twitter.com/share" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+				<p>Next time you complain that you don’t know what your passion is or are wondering where to apply your creativity, count your blessings you didn’t have go through a horrifying bus accident to find some clarity.</p><p>
+By all accounts, the young Frida Kahlo’s career plan was to become a doctor. She was a bright young woman and attended Mexico’s prestigious National Preparatory School where she was only one of 35 female students among 2000. She was in her senior year and making plans to attend medical school when the fateful day of the bus accident occurred.</p><p>
+Yes, she had dabbled in art before. Frida’s father, whom she greatly admired, was an amateur artist and she began to draw at age 12, but the accident and the isolation of her recovery changed Frida. Trapped in bed for months, she re-evaluated not only herself but the world around her. With the help of a mirror placed above her bed, Frida began painting self-portraits, something she would do for the rest of her life, constantly examining herself and looking inward. After the tragedy of the accident Frida was reborn and had found new life in painting: “From that time my obsession was to begin again, painting things just as I saw them with my own eyes and nothing more.”</p><p>
+It’s an origin story befitting a superhero, and Frida continued to live a heroic life despite decades of more agony and suffering (shortly after her exhibition in Mexico, her right leg had to be amputated below the knee due to gangrene). She really was “la heroina del dolor”, the Heroine of Pain.</p>
+</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer" class="container">
+
+	<div class="col-md-12">
+			<div class="row strip">@developed by anjali</div>
+		</div>
+</body>
+</html>
